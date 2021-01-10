77a3f76a653bbb21702ff86971d1d4c66cde8a0a
Brandon Piercy <b.piercy567@gmail.com>
2021-01-10 12:27:21 -0800

    connected to mongoose

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 7777;
 
-const db = require("./models");
-
 const app = express();
 
 app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {useNewUrlParser: true});
