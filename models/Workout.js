fb6a81f304935e72735c7cdbfc58c0e9b22d15ee
Brandon Piercy <b.piercy567@gmail.com>
2021-01-10 13:12:59 -0800

    workout model is complete (?) (still non-functional)

--- models/Workout.js
+++ models/Workout.js
@@ -2,7 +2,42 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema(
-    {}
+    {
+        day: {
+            type: Date,
+            default: () => new Date()
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    require: "Select the exercise type.",
+                    trim: true
+                },
+                name: {
+                    type: String,
+                    required: "Exercise needs a name.",
+                    trim: true
+                },
+                duration: {
+                    type: Number,
+                    required: "Enter the time spent, in minutes"
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+    }
 );
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
