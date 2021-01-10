989e8377718f043ece2b842224e7ddeb6f0171fa
Brandon Piercy <b.piercy567@gmail.com>
2021-01-10 14:41:02 -0800

    routing is complete but there is some type of object error

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,4 +10,24 @@ module.exports = (app) => {
             res.json(err)
         });
     });
+
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create(req.body)
+        .then(dbWorkout => {
+            console.log(dbWorkout);
+        })
+        .catch(({message}) => {
+            console.log(message);
+        });
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}})
+        .then(dbWorkout => {
+            console.log(dbWorkout);
+        })
+        .catch(({message}) => {
+            console.log(message);
+        });
+    });
 };
