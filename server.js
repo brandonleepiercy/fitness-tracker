cd7bb196503736fd52742bacb19d6d7b7b89e01a
Brandon Piercy <b.piercy567@gmail.com>
2021-01-10 12:41:06 -0800

    finished server file (doesn't spin yet)

--- server.js
+++ server.js
@@ -14,3 +14,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker", {useNewUrlParser: true});
+
+require("./routes/htmlRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT: ${PORT}`);
+});
