d9700a35c2df58873105bc42ca32d10128e82e6a
Brandon Piercy <b.piercy567@gmail.com>
2021-01-10 13:16:35 -0800

    added one api route to test

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,13 @@
 const db = require("../models");
 
 module.exports = (app) => {
-
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.json(err)
+        });
+    });
 };
